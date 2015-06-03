73375a21d900c69cafd9050d5a785d56316c0f60
Angie <angi3yang@gmail.com>
2015-06-03 13:20:19 -0400

    Finished 5 specs

--- models/task.model.js
+++ models/task.model.js
@@ -15,25 +15,32 @@ var TaskSchema = new Schema({
 //virtuals
 
 TaskSchema.virtual('timeRemaining').get(function() {
-  
+	if(!this.due) return Infinity;
+	var d = new Date();
+	return Math.abs(d.getTime()-this.due.getTime());
+	
 })
 
 TaskSchema.virtual('overdue').get(function() {
-
+	var d = new Date();
+	if(!this.complete) return this.due.getTime() < d.getTime();
+	else return false;
 })
 
 //methods
 
 TaskSchema.methods.addChild = function(params) {
- 
+	console.log(this.create(params))
+	return this.create(params);
+	// return this.Task.insert(params).then();
 }
 
 TaskSchema.methods.getChildren = function() {
-
+	return this.find();
 }
 
 TaskSchema.methods.getSiblings = function() {
-
+	return this.find({parent : {$eq: parent}})
 }
 
 Task = mongoose.model('Task', TaskSchema);
